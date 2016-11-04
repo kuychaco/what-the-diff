ecf4ace24831fec24f80c12a9f3ccaf85e720598
Katrina Uychaco <kuychaco@gmail.com>
2016-11-03 18:12:02 -0700

    :art: use dedent-js

--- test/diff.test.js
+++ test/diff.test.js
@@ -1,18 +1,21 @@
 var diff = require('../')
+var dedent = require('dedent-js')
 var fs = require('fs')
 
 var assert = require("nodeunit").assert
 
 exports.testSimplePatch = function(test) {
-  var str = `diff --git file.txt file.txt
-index 83db48f..bf269f4 100644
---- file.txt
-+++ file.txt
-@@ -1,3 +1,3 @@
- line1
--line2
-+new line
- line3`
+  var str = dedent`
+    diff --git file.txt file.txt
+    index 83db48f..bf269f4 100644
+    --- file.txt
+    +++ file.txt
+    @@ -1,3 +1,3 @@
+     line1
+    -line2
+    +new line
+     line3
+  `
 
   const output = diff.parse(str)
   assert.deepEqual(output, [
@@ -42,13 +45,15 @@ index 83db48f..bf269f4 100644
 }
 
 exports.testNewPatch = function(test) {
-  var str = `diff --git file.txt file.txt
-new file mode 100644
-index 0000000..dab621c
---- /dev/null
-+++ file.txt
-@@ -0,0 +1 @@
-+foo`
+  var str = dedent`
+    diff --git file.txt file.txt
+    new file mode 100644
+    index 0000000..dab621c
+    --- /dev/null
+    +++ file.txt
+    @@ -0,0 +1 @@
+    +foo
+  `
 
   const output = diff.parse(str)
   assert.deepEqual(output, [
@@ -73,13 +78,15 @@ index 0000000..dab621c
 }
 
 exports.testRemovedPatch = function(test) {
-  var str = `diff --git file.txt file.txt
-deleted file mode 100644
-index dab621c..0000000
---- file.txt
-+++ /dev/null
-@@ -1 +0,0 @@
--foo`
+  var str = dedent`
+    diff --git file.txt file.txt
+    deleted file mode 100644
+    index dab621c..0000000
+    --- file.txt
+    +++ /dev/null
+    @@ -1 +0,0 @@
+    -foo
+  `
 
   const output = diff.parse(str)
   assert.deepEqual(output, [
@@ -104,17 +111,19 @@ index dab621c..0000000
 }
 
 exports.testFileModeChange = function(test) {
-  var str = `diff --git file.txt file.txt
-old mode 100644
-new mode 100755
-index 83db48f..bf269f4
---- file.txt
-+++ file.txt
-@@ -1,3 +1,3 @@
- line1
--line2
-+new line
- line3`
+  var str = dedent`
+    diff --git file.txt file.txt
+    old mode 100644
+    new mode 100755
+    index 83db48f..bf269f4
+    --- file.txt
+    +++ file.txt
+    @@ -1,3 +1,3 @@
+     line1
+    -line2
+    +new line
+     line3
+  `
 
   const output = diff.parse(str)
   assert.deepEqual(output, [
@@ -144,9 +153,11 @@ index 83db48f..bf269f4
 }
 
 exports.testNewEmptyFile = function(test) {
-  var str = `diff --git newfile.txt newfile.txt
-new file mode 100644
-index 0000000..e69de29`
+  var str = dedent`
+    diff --git newfile.txt newfile.txt
+    new file mode 100644
+    index 0000000..e69de29
+  `
 
   const output = diff.parse(str)
   assert.deepEqual(output, [
@@ -163,13 +174,15 @@ index 0000000..e69de29`
 }
 
 exports.testSingleLineHunk = function(test) {
-  var str = `diff --git file.txt file.txt
-index 83db48f..bf269f4 100644
---- file.txt
-+++ file.txt
-@@ -1 +1 @@
--line1
-+line2`
+  var str = dedent`
+    diff --git file.txt file.txt
+    index 83db48f..bf269f4 100644
+    --- file.txt
+    +++ file.txt
+    @@ -1 +1 @@
+    -line1
+    +line2
+  `
 
   const output = diff.parse(str)
   assert.deepEqual(output, [
@@ -197,22 +210,24 @@ index 83db48f..bf269f4 100644
 }
 
 exports.testMultipleHunks = function(test) {
-  var str = `diff --git file.txt file.txt
-index 83db48f..bf269f4 100644
---- file.txt
-+++ file.txt
-@@ -1,5 +1,4 @@
- line1
--line2
- line3
- line4
- line5
-@@ -15,4 +14,5 @@
- line6
- line7
- line8
-+line2
- line9`
+  var str = dedent`
+    diff --git file.txt file.txt
+    index 83db48f..bf269f4 100644
+    --- file.txt
+    +++ file.txt
+    @@ -1,5 +1,4 @@
+     line1
+    -line2
+     line3
+     line4
+     line5
+    @@ -15,4 +14,5 @@
+     line6
+     line7
+     line8
+    +line2
+     line9
+  `
 
   const output = diff.parse(str)
   assert.deepEqual(output, [
@@ -256,14 +271,16 @@ index 83db48f..bf269f4 100644
 }
 
 exports.testRemovedEOFNL = function(test) {
-  var str = `diff --git file.txt file.txt
-index a999a0c..266014b 100644
---- file.txt
-+++ file.txt
-@@ -1 +1 @@
--line
-+line
-\ No newline at end of file`
+  var str = dedent`
+    diff --git file.txt file.txt
+    index a999a0c..266014b 100644
+    --- file.txt
+    +++ file.txt
+    @@ -1 +1 @@
+    -line
+    +line
+    \ No newline at end of file
+  `
 
   const output = diff.parse(str)
   assert.deepEqual(output, [
@@ -292,14 +309,16 @@ index a999a0c..266014b 100644
 }
 
 exports.testAddedEOFNL = function(test) {
-  var str = `diff --git file.txt file.txt
-index 266014b..a999a0c 100644
---- file.txt
-+++ file.txt
-@@ -1 +1 @@
--line
-\ No newline at end of file
-+line`
+  var str = dedent`
+    diff --git file.txt file.txt
+    index 266014b..a999a0c 100644
+    --- file.txt
+    +++ file.txt
+    @@ -1 +1 @@
+    -line
+    \ No newline at end of file
+    +line
+  `
 
   const output = diff.parse(str)
   assert.deepEqual(output, [
@@ -328,15 +347,17 @@ index 266014b..a999a0c 100644
 }
 
 exports.testEmptyHunkLine = function(test) {
-  var str = `diff --git file.txt file.txt
-index 83db48f..bf269f4 100644
---- file.txt
-+++ file.txt
-@@ -1,3 +1,3 @@
- line1
--line2
-+
- line3`
+  var str = dedent`
+    diff --git file.txt file.txt
+    index 83db48f..bf269f4 100644
+    --- file.txt
+    +++ file.txt
+    @@ -1,3 +1,3 @@
+     line1
+    -line2
+    +
+     line3
+  `
 
   const output = diff.parse(str)
   assert.deepEqual(output, [
@@ -366,20 +387,22 @@ index 83db48f..bf269f4 100644
 }
 
 exports.testMergeConflicts = function(test) {
-  var str = `diff --cc modified-on-both.txt
-index 5b7855c,1353022..0000000
---- modified-on-both.txt
-+++ modified-on-both.txt
-@@@ -1,1 -1,1 +1,7 @@@
-++<<<<<<< HEAD
- +master modification
-++||||||| merged common ancestors
-++text
-++=======
-+ branch modification
-++>>>>>>> branch
-* Unmerged path removed-on-branch.txt
-* Unmerged path removed-on-master.txt`
+  var str = dedent`
+    diff --cc modified-on-both.txt
+    index 5b7855c,1353022..0000000
+    --- modified-on-both.txt
+    +++ modified-on-both.txt
+    @@@ -1,1 -1,1 +1,7 @@@
+    ++<<<<<<< HEAD
+     +master modification
+    ++||||||| merged common ancestors
+    ++text
+    ++=======
+    + branch modification
+    ++>>>>>>> branch
+    * Unmerged path removed-on-branch.txt
+    * Unmerged path removed-on-master.txt
+  `
 
   const output = diff.parse(str)
   assert.deepEqual(output, [
