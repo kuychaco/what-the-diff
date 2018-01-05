638c95e3e7557cb9ed869b66f50e49f80cb51aa7
Katrina Uychaco <kuychaco@gmail.com>
2018-01-05 13:35:46 -0800

    Don't log parse error

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ var parser = require('./lib/parser')
 
 module.exports = {
   parse: function(input) {
-    try {
-      return parser.parse(input.toString())
-    } catch (e) {
-      console.log(e.location)
-      throw e
-    }
+    return parser.parse(input.toString())
   }
 }
